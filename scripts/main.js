6ff6483002853303cbc8514b8e5213b6b3447bd0
suzannahurst <hurstsuzanna@gmail.com>
2024-04-19 15:07:33 +0100

    read styling, new menu

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const paragraph4 = document.getElementById("paragraph4");
   const paragraph5 = document.getElementById("paragraph5");
   const paragraph6 = document.getElementById("paragraph6");
+  const paragraph7 = document.getElementById("paragraph7");
+  const paragraph8 = document.getElementById("paragraph8");
 
   gsap.from(paragraph1, { duration: 0.1, opacity: 0, delay: 0.2 });
   gsap.from(paragraph2, { duration: 0.1, opacity: 0, delay: 2 });
@@ -12,4 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
   gsap.from(paragraph4, { duration: 0.1, opacity: 0, delay: 6 });
   gsap.from(paragraph5, { duration: 0.1, opacity: 0, delay: 8 });
   gsap.from(paragraph6, { duration: 0.1, opacity: 0, delay: 10 });
+  gsap.from(paragraph7, { duration: 0.1, opacity: 0, delay: 12 });
+  gsap.from(paragraph8, { duration: 0.1, opacity: 0, delay: 14 });
 });
